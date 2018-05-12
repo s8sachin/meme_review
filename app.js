805f7ec41453ebfe386382e85edaae0ea124b833
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-05-13 01:20:01 +0530

    cache fixes

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 3600000 }));
 
 app.use('/', index);
 app.use('/users', users);
