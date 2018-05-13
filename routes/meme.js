f989ca3c3eff4f7246c95ea96720203fe75a573b
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-05-13 17:44:18 +0530

    bootccards and memes index

--- routes/meme.js
+++ routes/meme.js
@@ -4,6 +4,9 @@ var meme = require('../controllers/MemeController.js');
 
 // new meme
 router.get('/new_meme', meme.new);
+
 router.post('/new_meme', meme.create);
+
+router.get('/list', meme.index);
 
 module.exports = router;
