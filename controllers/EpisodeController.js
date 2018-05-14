e46acbe970b629f0c7611fb4d34dc0296ee1b887
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-05-14 15:07:42 +0530

    meme review saving and incrementing

--- controllers/EpisodeController.js
+++ controllers/EpisodeController.js
@@ -9,8 +9,19 @@ episodesController.new = function(req, res) {
 };
 
 episodesController.create = function(req, res) {
-  console.log("hii")
-  res.redirect('/episode/list')
+  youtube_url = req.body.meme_review_episode_url;
+  youtube_vid_id = youtube_url.split("watch?v=")[1];
+  meme_review = {
+    meme_review_episode_url : youtube_url,
+    meme_review_episode_thumb : `https://img.youtube.com/vi/${youtube_vid_id}/0.jpg`
+  }
+  new MemeReview(meme_review)
+  .save(err => {
+    if (err) {
+      return console.log(err);
+    };
+    res.redirect('/episode/list');
+  });
 };
 
 episodesController.index = function(req, res) {
--- models/MemeReview.js
+++ models/MemeReview.js
@@ -1,12 +1,9 @@
 var mongoose = require('mongoose');
 var timestamps = require('mongoose-timestamp');
+var autoIncrement = require('mongoose-ai');
 var Schema = mongoose.Schema;
 
 var MemeReviewSchema = new Schema({
-  meme_review_episode_num: {
-    type: Number,
-    required: true
-  },
   meme_review_episode_url: {
     type: String,
     required: true
@@ -18,4 +15,11 @@ var MemeReviewSchema = new Schema({
 });
 
 MemeReviewSchema.plugin(timestamps);
+autoIncrement.initialize(mongoose.connection);
+MemeReviewSchema.plugin(autoIncrement.plugin, {
+    model: 'MemeReview',
+    field: 'meme_review_episode_num',
+    startAt: 1,
+    incrementBy: 1
+});
 module.exports = mongoose.model('MemeReview', MemeReviewSchema);
