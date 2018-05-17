9e1bce984985d2b6266624cd45db218ffdd599ae
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-05-17 22:27:32 +0530

    added projection

--- controllers/api/MemeController.js
+++ controllers/api/MemeController.js
@@ -7,11 +7,14 @@ var memeController = {};
 memeController.index = function(req, res) {
   start = req.query.start || 0;
   show = req.query.show || 0;
+  projection = {_id: 0, __v: 0, createdAt: 0, updatedAt: 0};
   Meme.find({})
-  .populate('meme_review', 'meme_review_episode_num meme_review_episode_url')
+  .select(projection)
+  .populate('meme_review', projection)
   .then(memes => {
     res.json({'memes': memes});
-  });
+  })
+  .catch(e => res.status(500).json({'error': e.message}));
 };
 
 function set_image(image_url, size){
